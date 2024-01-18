cac67299aacca6c1788e2ee4864354521d2e52be
Frier03 <peter.frier12345@gmail.com>
2024-01-18 20:33:42 +0100

    Initial Commit

---
+++ Inventar/InventarApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventarApp.swift
+//  Inventar
+//
+//  Created by Peter Frier on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct InventarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
